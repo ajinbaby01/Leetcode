f78cf3665ebc24f024a2cdc85b4575c8be9c53b5
ajinbaby01 <ajinbaby941@gmail.com>
2023-11-16 10:51:08 +0530

    Small improvement

--- 5. Recursion and Backtracking/combinations.py
+++ 5. Recursion and Backtracking/combinations.py
@@ -3,19 +3,20 @@ from typing import List
 
 class Solution:
     def combine(self, nums: List, k: int) -> List[List[int]]:
-        def recursion(nums, start, comb=[], answer=[]):
-            nums.sort()
+        nums.sort()
+        def recursion(nums, comb=[], answer=[]):
             if len(comb) == k:
                 answer.append(comb[:])
+                return
 
             visited = set()
 
-            for i in range(start, len(nums)):
+            for i in range(len(nums)):
                 if nums[i] not in visited:
                     visited.add(nums[i])
                     comb.append(nums[i])
-                    recursion(nums, i+1, comb)
+                    recursion(nums[i+1:])
                     comb.pop()
             return answer
-        return recursion(nums, 0)
+        return recursion(nums)
 print(Solution().combine([4,4,4,1,4], 2))
