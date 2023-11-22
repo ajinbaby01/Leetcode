c1b19142df193c020173d14780c142742699e5ae
ajinbaby01 <ajinbaby941@gmail.com>
2023-11-22 15:23:13 +0530

    Graph Traversal: BFS & DFS

---
+++ 13. Graph/BFS_Traversal.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+res = []
+
+def printGraph(matrix):
+    for i in range(len(matrix)):
+      print()
+      for j in range(len(matrix[0])):
+        print(matrix[i][j], end=" ")
+    print()
+
+def bfs(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    visited = set()
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def traverse(i, j):
+       queue = deque([(i, j)])
+
+       while queue:
+          i, j = queue.popleft()
+          if (i, j) in visited:
+             continue
+
+          visited.add((i, j))
+          res.append(matrix[i][j])
+
+          for direction in directions:
+             next_i, next_j = i + direction[0], j + direction[1]
+             if next_i in range(rows) and next_j in range(cols):
+                queue.append((next_i, next_j))
+
+    for i in range(rows):
+       for j in range(cols):
+          traverse(i, j)
+
+matrix = [["1","2","3","4","5"],["6","7","8","9","10"],["11","12","13","14","15"],["16","17","18","19","20"]]
+bfs(matrix)
+print(res)
---
+++ 13. Graph/DFS_Traversal.py
@@ -0,0 +1,33 @@
+res = []
+
+def printGraph(matrix):
+    for i in range(len(matrix)):
+      print()
+      for j in range(len(matrix[0])):
+        print(matrix[i][j], end=" ")
+    print()
+
+def dfs(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    visited = set()
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def traverse(i, j):
+        if (i, j) in visited:
+           return
+
+        visited.add((i, j))
+        res.append(matrix[i][j])
+
+        for direction in directions:
+           next_i, next_j = i + direction[0], j + direction[1]
+           if next_i in range(rows) and next_j in range(cols):
+              traverse(next_i, next_j)
+
+    for i in range(rows):
+       for j in range(cols):
+          traverse(i, j)
+
+matrix = [["1","2","3","4","5"],["6","7","8","9","10"],["11","12","13","14","15"],["16","17","18","19","20"]]
+dfs(matrix)
+print(res)
