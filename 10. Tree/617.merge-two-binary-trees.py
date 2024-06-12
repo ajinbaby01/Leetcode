11f472d860368c110b3eaa21226db000e7ad4de3
ajinbaby01 <ajinbaby941@gmail.com>
2024-06-12 22:17:25 +0530

    Add iterative approach

--- 10. Tree/617.merge-two-binary-trees.py
+++ 10. Tree/617.merge-two-binary-trees.py
@@ -59,6 +59,10 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        # return self.recursive(root1, root2)
+        return self.iterative(root1, root2)
+    
+    def recursive(self, root1, root2):
         # If both root1 and root2 exists, take their sum and update root1.
         # Update left and right child of root1 with either the sum or one of the existing nodes if other is None,
         # since if either root1 or root2 is None, not None root is returned.
@@ -69,4 +73,37 @@ class Solution:
             return root1
         else:
             return root1 or root2
+    
+    def iterative(self, root1, root2):
+        # If one tree is empty, return the other
+        if not root2 or not root1:
+            return root1 or root2
+        
+        stack = [(root1, root2)]
+        while stack:
+            t1, t2 = stack.pop()
+
+            # We update the nodes of t1.
+            # If t2 is empty, nodes are not overlapping and we can skip.
+            if not t2:
+                continue
+            
+            # Add the values of overlapping nodes together
+            t1.val += t2.val
+
+            # We only add childern of t1(t1.left/t1.right) to stack if it exists.
+            # If child is None, we place the child of t2(t2.left/t2.right) in that position.
+            # Child of t2(t2.left/t2.right) can be None. In that case, tree remains unchanged.
+            # Eg: t1.left = t2.left => t1.left = None. t1.left was already None(tree unchanged)
+            if t1.left:
+                stack.append((t1.left, t2.left))
+            else:
+                t1.left = t2.left
+            
+            if t1.right:
+                stack.append((t1.right, t2.right))
+            else:
+                t1.right = t2.right
+                
+        return root1
 # @lc code=end
