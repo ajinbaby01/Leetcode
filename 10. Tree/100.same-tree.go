acb77c7c298b0664dfbaf0b2e7c735d3a5a1f67c
ajinbaby01 <ajinbaby941@gmail.com>
2025-08-27 16:41:28 +0530

    100. Same Tree : Golang

---
+++ 10. Tree/100.same-tree.go
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=100 lang=golang
+ *
+ * [100] Same Tree
+ *
+ * https://leetcode.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (62.38%)
+ * Likes:    12382
+ * Dislikes: 273
+ * Total Accepted:    2.9M
+ * Total Submissions: 4.5M
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * Given the roots of two binary trees p and q, write a function to check if
+ * they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally identical,
+ * and the nodes have the same value.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in both trees is in the range [0, 100].
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+// @lc code=end
