247c5232492ec6b4fb7d4396d072941006af317a
ajinbaby01 <ajinbaby941@gmail.com>
2025-09-18 00:14:54 +0530

    118. Pascal's Triangle

---
+++ 118.pascals-triangle.go
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=118 lang=golang
+ *
+ * [118] Pascal's Triangle
+ *
+ * https://leetcode.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (74.53%)
+ * Likes:    14463
+ * Dislikes: 541
+ * Total Accepted:    2.4M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '5'
+ *
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it as shown:
+ *
+ *
+ * Example 1:
+ * Input: numRows = 5
+ * Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ * Example 2:
+ * Input: numRows = 1
+ * Output: [[1]]
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= numRows <= 30
+ *
+ *
+ */
+
+// @lc code=start
+func generate(numRows int) [][]int {
+    pascalTri := [][]int{
+        []int{1},
+    }
+    rowCount := 1
+    for rowCount < numRows {
+        row := []int{1}
+        for i := 1; i < rowCount; i++ {
+			row = append(row, pascalTri[rowCount - 1][i] + pascalTri[rowCount - 1][i-1])
+        }
+        row = append(row, 1)
+        pascalTri = append(pascalTri, row)
+		rowCount++
+    }
+	return pascalTri
+}
+// @lc code=end
